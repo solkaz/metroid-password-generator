a2e0154e681e9acb6d873d469b93bd40ce4d1bbd
JMH <jheld135@gmail.com>
2017-02-04 22:16:56 -0600

    Added ALL the options

--- src/components/DataModifiers.jsx
+++ src/components/DataModifiers.jsx
@@ -1,24 +1,12 @@
 import React, { PropTypes } from 'react';
+
 import OptionToggler from './OptionToggler.jsx';
 import GameTime from './GameTime.jsx';
 import Missiles from './Missiles.jsx';
 import StartLocation from './StartLocation.jsx';
-import { bitsetToNumber } from '../utils.js';
 
-const optionsList = [
-  { title: 'Metroid Ball Taken', bitIndex: 0 },
-  { title: 'Missile Container (Brinstar)', bitIndex: 1 },
-  { title: 'Red Door (Long Beam)', bitIndex: 2},
-  { title: 'Red Door (Tourian Bridge)', bitIndex: 3},
-  { title: 'Samus has bombs', bitIndex: 72},
-  { title: 'Samus has high jump boots', bitIndex: 73},
-  { title: 'Samus has Long Beam', bitIndex: 74},
-  { title: 'Samus has Screw Attack', bitIndex: 75},
-  { title: 'Samus has Metroid Ball', bitIndex: 76},
-  { title: 'Samus has Varia', bitIndex: 77},
-  { title: 'Samus has Wave Beam', bitIndex: 78},
-  { title: 'Samus has Ice Beam', bitIndex: 79},
-];
+import { bitsetToNumber } from '../utils.js';
+import optionsList from '../optionsList.js';
 
 class DataModifiers extends React.Component {
   render() {
